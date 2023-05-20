e762cc823104fddf77113344a6b89afb46da4acd
Aleksandr Mineev <175aleksandr175@gmail.com>
2023-05-20 18:23:58 +0300

    twenty-fourth lesson

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,5 +3,6 @@ package main
 import "github.com/Aleksandr17552/snippetbox/pkg/models"
 
 type templateData struct {
-	Snippet *models.Snippet
+	Snippet  *models.Snippet
+	Snippets []*models.Snippet
 }
